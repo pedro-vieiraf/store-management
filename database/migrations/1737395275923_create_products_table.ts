30b464d62dae85d6f9346af3fe65c461bfc5aa10
Pedro Vieira <pedrovieirafdev@gmail.com>
2025-01-20 15:02:14 -0300

    feat: products model, migration and seeder created

--- database/migrations/1737395275923_create_products_table.ts
+++ database/migrations/1737395275923_create_products_table.ts
@@ -1,16 +1,16 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'addresses'
+  protected tableName = 'products'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('country', 100).notNullable()
-      table.string('state', 100).notNullable()
-      table.string('city', 100).notNullable()
-      table.string('street', 100).notNullable()
-      table.integer('number').notNullable()
+      table.string('name').notNullable()
+      table.string('description')
+      table.decimal('price', 10, 2).notNullable()
+      table.integer('stock').notNullable()
+
       table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
       table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
